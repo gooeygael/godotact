a63714e5a3ea4f4885d3e44d91de5039a10b0eca
Steven <stevensuchdolski@gmail.com>
2025-06-11 23:05:43 -0400

    tilemap removal

--- Scripts/BattleMap.gd
+++ Scripts/BattleMap.gd
@@ -29,62 +29,10 @@ func _ready():
 	generate_grid()
 	spawn_units()
 
-
-
-func highlight_range(center: Vector2i, range: int):
-		for x in range(range * 2 + 1):
-				for y in range(range * 2 + 1):
-						var pos = center + Vector2i(x - range, y - range)
-						if tiles.has(pos):
-								var tile_node = get_tile_node_at(pos)
-								if tile_node:
-										tile_node.highlight()
-
 func get_tile_node_at(pos: Vector2i) -> Node2D:
 		if visual_tiles.has(pos):
 			return visual_tiles[pos]
 		return null
-
-	
-func show_movement_range(origin: Vector2i, range: int):
-	clear_highlights()
-
-	var open := [origin]
-	var visited := {}
-	var directions := [Vector2i.UP, Vector2i.DOWN, Vector2i.LEFT, Vector2i.RIGHT]
-
-	while open.size() > 0:
-		var current = open.pop_front()
-		if visited.has(current):
-			continue
-		visited[current] = true
-
-		var distance = abs(current.x - origin.x) + abs(current.y - origin.y)
-		if distance > range:
-			continue
-
-		var tile_node = get_tile_node_at(current)
-		if tile_node:
-			tile_node.highlight()
-			movement_range_tiles.append(tile_node)
-
-		for dir in directions:
-			var neighbor = current + dir
-			if tiles.has(neighbor) and tiles[neighbor]["walkable"]:
-				open.append(neighbor)
-
-
-func clear_highlights():
-	for tile in movement_range_tiles:
-		tile.reset()
-	movement_range_tiles.clear()
-	
-	
-func select_unit(unit: Node):
-	selected_unit = unit
-	show_movement_range(unit.grid_pos, unit.move_range)
-	
-
 
 func generate_grid():
 	for x in state.grid_width:
